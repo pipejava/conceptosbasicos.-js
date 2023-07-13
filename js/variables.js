b9c3bfb5898b8e07a2a5cdb6e0f0357e11ee1fd0
pijava <ingandres0203@gmail.com>
2023-07-12 21:14:42 -0500

    120723

---
+++ js/variables.js
@@ -0,0 +1,17 @@
+/** 
+ * para declarar variables se utiliza var, let y const
+ * tipos de datos: null, undenifed, string(texto), boolean(true or false), number, symbol y object
+*/
+
+let miEdad=22;//dato tipo número
+
+const miNombre = 'Andrés';
+
+const casado = false;
+
+//miNombre='Fausto';
+
+console.log('Mi nombre:'+ miNombre);
+console.log('Mi edad:'+ miEdad);
+console.log('Estado Civil:'+ casado);
+
