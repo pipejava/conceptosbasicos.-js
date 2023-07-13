30514f189e51344dfa721819dcad911a7e2f4ac5
pijava <ingandres0203@gmail.com>
2023-07-12 20:51:07 -0500

    app-hola mundo

---
+++ js/hola-mundo.js
@@ -0,0 +1,8 @@
+//primera de app en javascrip
+/**
+ * app hola mundo utiliza un función llamada console.log
+ * 
+ */
+//muestra el resultado en la consola del navegador
+console.log('Hola mundo');
+
