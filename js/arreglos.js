52c5a4fac73b3e8a7e0386243f2ed5fc69720bce
pijava <ingandres0203@gmail.com>
2023-07-14 18:29:48 -0500

    140723

---
+++ js/arreglos.js
@@ -0,0 +1,13 @@
+// los arreglos sirven para contener varias variables
+// sintaxis se declara como una variable let o const 
+
+const auto = [];// array o arreglo vacio 
+let  Auto = ['Deportivo', 'rojo', 'automatico', 250000000];
+
+// mostrar los datos del arreglo
+console.log(auto);
+console.log({Auto});
+
+// ejemplo 2
+
+const movies=['flash','Indiana Jhones','Elementos','Mision imposible'];
