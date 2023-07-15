7f092240a90da77f3e0ab4ee2b375b0860128eaa
pijava <ingandres0203@gmail.com>
2023-07-14 20:40:46 -0500

    140723

--- js/arreglos.js
+++ js/arreglos.js
@@ -4,10 +4,50 @@
 const auto = [];// array o arreglo vacio 
 let  Auto = ['Deportivo', 'rojo', 'automatico', 250000000];
 
-// mostrar los datos del arreglo
+// mostrar los datos del Auto
 console.log(auto);
 console.log({Auto});
 
 // ejemplo 2
 
 const movies=['flash','Indiana Jhones','Elementos','Mision imposible'];
+//mostrar todas las pelis
+console.log(movies);
+//mostrar una posicion del array
+console.log(movies[1]);
+
+//combinar arrays
+let libros = [
+    'Sistemas',
+    'Contabilidad',
+    25000,
+    37000,
+    25000/2,
+    ['Wilson','Pedro','Juanito',true, '14/07/23']
+
+]
+
+//mostrar la información de los libros
+console.log(libros);
+//mostrar la fecha 
+console.log(libros[5][3]);
+
+//muestra el último valor del arreglo
+
+let ultimoValor = libros[libros.length - 1];
+console.log('El ultimo valor es: ', ultimoValor);
+
+// cuantós valores tiene el array de libros
+console.log('Los valores:',libros.leght);
+
+//agregar un nuevo elemento al array de libros
+let nuevoLibro = libros.push('El Principito');
+console.log({nuevoLibro, libros});
+
+//agregar nuevo elemento al inicio del array de libros
+let inicioLibro = libros.unshift('Crimen  y castigo');
+console.log({inicioLibro, libros});
+
+//Eliminar un elemento del array de libros
+let borrarPosicion = libros.splice(5);
+console.log({borrarPosicion, libros});
---
+++ js/objetos-literales.js
@@ -0,0 +1,45 @@
+//sintaxis de un objeto
+//con llaves es un objeto
+let libros = {
+    libro1: 'La fundacion',
+    libro2: 'El principito',
+    libro3: 'habitos atómicos',
+    precio1: 60000,
+    precio2: 45000,
+    precio3: 220000,
+    stock: true,
+
+    
+};
+
+//muestra todas las propiedades del objeto libros
+console.table(libros);
+
+//mostrar la primera propiedad del objeto libros
+console.log('El libro es: ', libros.libro1);
+
+//consultar el elemento precio1 del objeto
+const p1 = 'precio1';
+console.log('El precio 1 es: ', libros[p1]);
+
+//agregar una nueva propiedad al objeto libros
+libros.libro4 = 'Una mujer de 4 en conducta';
+console.table(libros);
+
+// Eliminar una propiedad del objeto libros
+delete libros.libro2; 
+console.table(libros);
+
+//combinar un objeto con arreglos
+let autos = {
+    marca1: 'Ferrari',
+    marca2: 'Toyota',
+    marca3: 'Mercedez',
+    paisMarca:['Italia', 'Japón', 'Alemania'],
+
+
+}
+
+//mostrar los paises
+
+console.table(autos['paisMarca'])
